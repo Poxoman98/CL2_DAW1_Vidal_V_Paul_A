370b1492c6cbefab8aea5dcfc3f6f928f050cbb6
Poxoman98 <paul_themagical@hotmail.com>
2024-12-06 09:07:49 -0500

    complatado 001

---
+++ src/main/java/pe/cibertec/CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE/Cl2Daw1VidalVivancoPaulAndreApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2Daw1VidalVivancoPaulAndreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2Daw1VidalVivancoPaulAndreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/cibertec/CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE/Cl2Daw1VidalVivancoPaulAndreApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2Daw1VidalVivancoPaulAndreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
