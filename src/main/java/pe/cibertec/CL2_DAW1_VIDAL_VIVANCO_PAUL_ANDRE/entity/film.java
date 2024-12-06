a4f3f19b59f5eacade102d8b9e6d3d0102406235
Poxoman98 <paul_themagical@hotmail.com>
2024-12-06 09:22:15 -0500

    complatado 003

---
+++ src/main/java/pe/cibertec/CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE/entity/film.java
@@ -0,0 +1,19 @@
+package pe.cibertec.CL2_DAW1_VIDAL_VIVANCO_PAUL_ANDRE.entity;
+
+import java.util.Date;
+
+public class film {
+    private Integer film_id;
+    private String title;
+    private String description;
+    private Integer release_year;
+    private Integer language_id;
+    private Integer original_language_id;
+    private Integer rental_duration;
+    private Double rental_rate;
+    private Integer length;
+    private Double replacement_cost;
+    private String rating;
+    private String special_features;
+    private Date last_update;
+}
